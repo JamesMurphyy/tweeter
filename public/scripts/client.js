2ae7f962e953ae0c3ca88021ddc5baa0565071c8
James Murphy <jamesm8228@gmail.com>
2022-03-03 21:01:52 -0500

    Made final edits to files and added comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// $('.fa-angle-double-down').scrollTo('.container')
-
-
-
-
-
-const escape = function (str) {
+//Escape function so text from user input does not harm the servers overall functionality.
+const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
-
+//Function that inputs a data object and returns HTML code with formatted information -- used for adding the tweets onto the page.
 const createTweetElement = function(tweet) {
   const $tweets = $(`
   <article> 
@@ -44,73 +39,67 @@ const createTweetElement = function(tweet) {
     </footer>
   </article>
   `);
-  
-  return $tweets
-}
+  return $tweets;
+};
 
+//Function deletes the tweets within the Tweets Container and then loops through the data that was given to then redisplay all the tweets, adding the new one each submission.
+//Preprend was used instead of append since the tweets need to be displayed newest to oldest.
 const renderTweets = function(tweets) {
   $('#tweetsContainer').empty();
   tweets.forEach(tweet => {
-    const $tweet = createTweetElement(tweet)
-    $('#tweetsContainer').prepend($tweet)
-  })
-  // $('#tweetsContainer').append(createTweetElement(tweet));
+    const $tweet = createTweetElement(tweet);
+    $('#tweetsContainer').prepend($tweet);
+  });
 };
 
-
+//Function used for initiating the ajax request upon submission from user on the Tweeter page. Upon success, the data is pushed through to the renderTweets function (aforementioned).
 const loadTweets = () => {
   $.ajax('/tweets', {
     method: 'GET',
     success: (tweets) => {
-      renderTweets(tweets)},
+      renderTweets(tweets);
+    },
     error: (data, text, error) => console.error(error)
   });
 };
 
-
+//Code for when the DOM is ready.
 $(document).ready(()=>{
 
+  //Function called to load the tweets that have already been given (from begining of assignment)
+  loadTweets();
   
-
-  $('.tweetText i').on('click', () => {
-    $('.new-tweet form').slideToggle();
-    $('.new-tweet textarea').focus();
-  });
-  
-
-  loadTweets()
-  
-  $( ".new-tweet form" ).submit(function( event ) {
+  //JQuery that submits the textarea information upon successful submission.
+  //A successful submission is when the "if" statements are correctly passed (text characters less than 140 and when there is something written).
+  $(".new-tweet form").submit(function(event) {
     event.preventDefault();
 
+    let counter = parseInt($(".counter").text());
+    let text = $("textarea").val();
 
-    let counter = parseInt($(".counter").text())
-    let text = $("textarea").val()
+    //Hides error and only calls it when an "if" statement fails.
     $(".error").hide();
-    console.log($(this).children(".error"))
 
     if (counter < 0) {
-      // return alert("Your message is over the character limit!")
       $(".tweet-error").text("Your tweet is over 140 characters. Please reduce this before tweeting!");
       $(this).children(".error").slideDown(300);
       
     } else if (text === null || text === "") {
-      // return alert("Your message box is empty!")
       $(".tweet-error").text("There is nothing being tweeted!");
       $(this).children(".error").slideDown(300);
 
     } else {
-
+      //When successful, ajax gets initiated and the data is then passed to the load tweets function. This creates the display of tweets and the addition of the one that was just submitted.
       $.ajax('/tweets', {
         data: $(this).serialize(),
         method: 'POST',
         success: (data) => {
-          loadTweets(data)
-          console.log($(this).serialize())
-          $("textArea").val(""); // clear textarea
-          $(".counter").text("140"); // reset counter to 140
+          loadTweets(data);
+          //Following the transfer of data, the textarea is then cleaned of its last message and the counter value is reset to 140.
+          $("textArea").val("");
+          $(".counter").text("140");
         }
-      })
+      });
     }
-  })
-})
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,15 @@
+//Code for when the DOM is ready.
 $(document).ready(function() {
   
+  //This jQuery is for calculating the text items and displaying the value.
   $(".new-tweet textarea").on("input", function() {
+
     let counter = $(this).siblings(".counter");
-    
-    let remainingChar = 140 - $(this).val().length
+    let remainingChar = 140 - $(this).val().length;
 
     counter.text(remainingChar);
-
+    //If statement is done after the calculation of the character value. If the character value is above 140, the counter class (determined in HTML document) adds the class "characterLimit"
+    //which was made within the new-tweet.css file giving the characters a red colour.
     if (remainingChar < 0) {
       counter.addClass("characterLimit");
     } else {
@@ -14,24 +17,29 @@ $(document).ready(function() {
     }
   });
 
+  //This jQuery is for the "Write a new Tweet" Button in the top right of the nav bar which hides or shows the textarea form.
+  $('.tweetText i').on('click', () => {
+    $('.new-tweet form').slideToggle();
+    $('.new-tweet textarea').focus();
+  });
 
-
+  //This jQuery if for the button that appears near the bottom of the screen (right side).
+  //This button only appears when the user scrolls a certain distance away from the top of the page.
   $("#button").hide();
-  $(window).scroll(function () {
+  $(window).scroll(function() {
     if ($(this).scrollTop() > 400) {
-        $('#button').fadeIn();
-        $('.tweetText').fadeOut();
+      $('#button').fadeIn();
+      $('.tweetText').fadeOut();
     } else {
-        $('#button').fadeOut();
-        $('.tweetText').fadeIn();
+      $('#button').fadeOut();
+      $('.tweetText').fadeIn();
     }
   });
 
-  $('#button').on('click', function(e) {
+  //This jQuery is for the button click, which then sends the user back to the top of the page.
+  $('#button').on('click', function() {
     $('html, body').animate({scrollTop:0}, '300');
   });
-
-
 });
 
 
